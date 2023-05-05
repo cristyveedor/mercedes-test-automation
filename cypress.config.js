95665d8c7f912ace7036d9ab1c8426b4ba4e65bb
Cristy Pearl Veedor <cristy.pearl.veedor@kramp.com>
2023-05-05 03:42:02 +0200

    Change the base url

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   e2e: {
     testIsolation: false,
     includeShadowDom: true,
-    baseUrl: 'https://www.mercedes-benz.co.uk/'
+    baseUrl: 'http://www.mercedes-benz.co.uk/'
   },
 });
